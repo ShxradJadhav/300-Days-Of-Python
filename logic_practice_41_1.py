675842da1b22c1c4e3f8c5193fdf9d62b3ad6714
ShxradJadhav <sharadjadhav1999@gmail.com>
2026-01-31 02:11:00 +0530

    Completed data logic task 41-1

---
+++ logic_practice_41_1.py
@@ -0,0 +1,10 @@
+"""
+Author: Sharad Jadhav
+Date: 2026-01-31
+Task: Calculate moving average for data sensor stream.
+"""
+
+def moving_avg(values, n=3):
+    return [sum(values[i:i+n])/n for i in range(len(values)-n+1)]
+
+# Execution check: Success
