427c4eaaa40e5919cf6e708873c4a6b64635b9eb
ShxradJadhav <sharadjadhav1999@gmail.com>
2026-02-01 02:22:00 +0530

    Completed data logic task 40-0

---
+++ logic_practice_40_0.py
@@ -0,0 +1,10 @@
+"""
+Author: Sharad Jadhav
+Date: 2026-02-01
+Task: Format SQL query string for daily data extraction.
+"""
+
+def get_query(table, date):
+    return f'SELECT * FROM {table} WHERE load_date = "{date}"'
+
+# Execution check: Success
