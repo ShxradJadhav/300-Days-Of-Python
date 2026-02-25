01cef78efebe9fe8d93c7eaa771a357528d76957
ShxradJadhav <sharadjadhav1999@gmail.com>
2026-02-26 02:18:00 +0530

    Completed data logic task 15-0

---
+++ logic_practice_15_0.py
@@ -0,0 +1,12 @@
+"""
+Author: Sharad Jadhav
+Date: 2026-02-26
+Task: Simple JSON configuration loader for Azure pipelines.
+"""
+
+import json
+def load_config(path):
+    with open(path, 'r') as f:
+        return json.load(f)
+
+# Execution check: Success
